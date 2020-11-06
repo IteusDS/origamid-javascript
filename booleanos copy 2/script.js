6c8dac9a23ddf719d132fa82720f74c9c6b83abe
IteusDS <mateusdsillva@gmail.com>
2020-11-06 13:02:29 -0300

    first commit

---
+++ booleanos copy 2/script.js
@@ -0,0 +1,35 @@
+var meuNomeE = false;
+
+if (meuNomeE){
+    console.log('Sim, meu nome é Mateus')} else{
+        console.log('Não, meu nome não é Mateus')
+    }
+
+ 
+
+    var nome ='Mateus';
+    var idade = 21;
+    var possuiDoutorado = false;
+    var empregoFuturo;
+    var dinheiroNaConta = 0;
+
+    var brasil = 207;
+    var china = 207;
+
+
+    console.log(!!nome, !!idade, !!possuiDoutorado, !!empregoFuturo, !!dinheiroNaConta)
+
+if (brasil === china) {
+    console.log("Brasil tem menos habitantes")
+
+}
+else{
+    console.log('China tem mais')
+}
+
+if (('Gato' === 'gato') && (5>2)){
+    console.log('Verdadeiro');
+} else {
+    console.log('Falso')
+}
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
---
+++ loop e array/script.js
@@ -0,0 +1,54 @@
+/*
+function tabuada1(){
+for (var tabuada = 0; tabuada <= 10; tabuada++){
+    console.log("1 x " + tabuada + " = " + 1*tabuada)
+}}
+
+
+function tabuada2(){
+for (var tabuada = 0; tabuada <= 10; tabuada++){
+    console.log("2 x " + tabuada + " = " + 2*tabuada)
+}}
+
+
+function tabuada3(){
+for (var tabuada = 0; tabuada <= 10; tabuada++){
+    console.log("3 x " + tabuada + " = " + 3*tabuada)
+}}
+
+
+function tabuada4(){
+for (var tabuada = 0; tabuada <= 10; tabuada++){
+    console.log("4 x " + tabuada + " = " + 4*tabuada)
+}}*/
+
+
+function tabuada5(){
+for (var tabuada = 0; tabuada <= 10; tabuada++){
+    console.log("5 x " + tabuada + " = " + 5*tabuada)
+}}
+
+
+function tabuada6(){
+for (var tabuada = 0; tabuada <= 10; tabuada++){
+    console.log("6 x " + tabuada + " = " + 6*tabuada)
+}}
+
+
+function tabuada7(){
+for (var tabuada = 0; tabuada <= 10; tabuada++){
+    console.log("7 x " + tabuada + " = " + 7*tabuada)
+}}
+
+
+function tabuada8(){
+for (var tabuada = 0; tabuada <= 10; tabuada++){
+    console.log("8 x " + tabuada + " = " + 8*tabuada)
+}}
+
+
+function tabuada9(){
+for (var tabuada = 0; tabuada <= 10; tabuada++){
+    console.log("9 x " + tabuada + " = " + 9*tabuada)
+}}
+
