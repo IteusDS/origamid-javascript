68ab514a72c07db9f55b7d11b995c0306c83eaa0
IteusDS <mateusdsillva@gmail.com>
2020-11-18 10:43:51 -0300

    first

--- DOM/dom-para-iniciantes/intro/script.js
+++ DOM/dom-para-iniciantes/intro/script.js
@@ -1,1 +1,10 @@
-window.alert()
+// Retorne o url da página atual utilizando o objeto window
+console.log(window.location)
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+document.querySelector('h1').innerHTML= "Heeeey";
+// Retorne a linguagem do navegador
+console.log(window.navigator.language)
+// Retorne a largura da janela 
+console.log(window.innerHeight)
+console.log(window.innerWidth)
