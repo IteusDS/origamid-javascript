e18e08f63da854fc88325f0750e1730c5ad71f98
IteusDS <mateusdsillva@gmail.com>
2020-11-26 14:10:26 -0300

    first commit

--- animaisteste/script.js
+++ animaisteste/script.js
@@ -1,4 +1,4 @@
-// Retorne no console todas as imagens do site
+/*// Retorne no console todas as imagens do site
 
 const imgall = document.querySelectorAll("img");
 console.log(imgall)
@@ -13,7 +13,37 @@ console.log(imglink)
 // Selecione o primeiro h2 dentro de .animais-descricao
 const h2text = document.querySelector('.animais-descricao h2');
 console.log(h2text)
-// Selecione o último p do site
+// Selecione o último p do site!!!
 
 const ptext = document.querySelector('.copy p');
-console.log(ptext)
+console.log(ptext);
+
+const ptextone =  document.querySelector( '')*/
+
+
+
+// Mostre no console cada parágrado do site
+
+let allp = document.querySelectorAll('p');
+console.log(allp);
+// Mostre o texto dos parágrafos no console
+allp.forEach((item) =>{
+    console.log('p')
+});
+
+
+// Como corrigir os erros abaixo:
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach(()=> {
+  console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach(() => {
+  console.log(i++)});
+
+imgs.forEach(() => {
+  i++})
+
+
